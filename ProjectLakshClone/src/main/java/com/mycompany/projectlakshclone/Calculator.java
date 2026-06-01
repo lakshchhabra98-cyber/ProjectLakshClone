3fc87a5fad535c8d5e4c9cdbb9622270ab3a4f41
lakshchhabra <lakshchhabra@localhost>
2026-06-01 16:16:42 -0400

    Add Calculator class with basic operations

---
+++ ProjectLakshClone/src/main/java/com/mycompany/projectlakshclone/Calculator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.projectlakshclone;
+
+/**
+ *
+ * @author Laksh Chhabra , 2026
+ */
+public class Calculator {
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
---
+++ ProjectLakshClone/src/main/java/com/mycompany/projectlakshclone/ProjectLakshClone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectlakshclone;
+
+/**
+ *
+ * @author Laksh Chhabra, 2026
+ */
+public class ProjectLakshClone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
